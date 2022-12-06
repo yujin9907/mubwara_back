4f55f456184f9ba08c199ef22193d5b62132bae9
yujin <heoyujin07@gmail.com>
2022-12-06 15:32:26 +0900

    subscribe 하기, 삭제 완료

--- src/main/java/site/metacoding/finals/domain/subscribe/SubscribeRepository.java
+++ src/main/java/site/metacoding/finals/domain/subscribe/SubscribeRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
 
+    public Subscribe findByCustomerId(Long customerId);
+
+    public void deleteByCustomerId(Long customerId);
 }
--- src/main/java/site/metacoding/finals/dto/subscribe/SubscribeRespDto.java
+++ src/main/java/site/metacoding/finals/dto/subscribe/SubscribeRespDto.java
@@ -1,5 +1,35 @@
 package site.metacoding.finals.dto.subscribe;
 
-public class SubscribeRespDto {
+import lombok.Getter;
+import lombok.Setter;
+import site.metacoding.finals.domain.customer.Customer;
+import site.metacoding.finals.domain.shop.Shop;
+import site.metacoding.finals.domain.subscribe.Subscribe;
 
+public class SubscribeRespDto {
+    @Setter
+    @Getter
+    public static class SubscribeSaveRespDto {
+        private Long id;
+        private Customer customer;
+        private ShopDto shopDto;
+
+        public SubscribeSaveRespDto(Subscribe subscribe) {
+            this.id = subscribe.getId();
+            this.customer = subscribe.getCustomer();
+            this.shopDto = new ShopDto(subscribe.getShop());
+        }
+
+        @Getter
+        public class ShopDto {
+            private long id;
+            private String shopName;
+
+            public ShopDto(Shop shop) {
+                this.id = shop.getId();
+                this.shopName = shop.getShopName();
+            }
+
+        }
+    }
 }
