b8e80eedc9f481bdd2cd786c1e5c050f1bf5d3c6
yujin9907 <heoyujin07@gmail.com>
2022-12-25 19:43:31 +0900

    userservice mock 테스트 환경 세팅

--- src/test/java/site/metacoding/finals/service/UserServiceTest.java
+++ src/test/java/site/metacoding/finals/service/UserServiceTest.java
@@ -10,7 +10,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import site.metacoding.finals.domain.reservation.ReservationRepository;
 
 @ExtendWith(MockitoExtension.class)
-public class ReservationServiceTest {
+public class UserServiceTest {
+
+    // https://galid1.tistory.com/772
 
     // 단위 테스트는 통합테스트보다 가볍게 진행할 수 있도록 springboottest 보다 mockito를 통해서 가볍게 테스트
 
@@ -26,7 +28,7 @@ public class ReservationServiceTest {
     }
 
     @Test
-    public void 타임리스트생성테스트() {
+    public void 유저중복체크테스트() {
 
     }
 
